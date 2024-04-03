76734e1a9b1f391125392c37a547bab19a27b14e
Erlang Andriyanputra <chilliope48@gmail.com>
2024-04-03 16:42:54 -0700

    fix image

--- src/components/FeatureComponent.vue
+++ src/components/FeatureComponent.vue
@@ -3,7 +3,7 @@
         <h2 class="text-2xl font-medium px-12">Featured Property</h2>
         <div class="w-full flex flex-col gap-4 lg:flex-row mt-10 lg:px-12">
             <div class="flex flex-col gap-4 lg:w-1/2 lg:h-full">
-                <div class="bg-[url('feature/1.avif')] w-full h-64 lg:w-full lg:h-96 bg-cover bg-center rounded-lg shadow-md px-4 py-3 flex items-end">
+                <div style="background-image: url('feature/1.avif');" class="w-full h-64 lg:w-full lg:h-96 bg-cover bg-center rounded-lg shadow-md px-4 py-3 flex items-end">
                     <div class="bg-white w-full h-max py-1 px-3 rounded-lg flex flex-row justify-between shadow-md items-center">
                         <div class="flex flex-col">
                             <p class="font-medium">Paradise Residence</p>
@@ -18,7 +18,7 @@
                 </div>
             </div>
             <div class="flex flex-col gap-4 lg:w-1/2">
-                <div class="bg-[url('feature/2.webp')] w-full h-36 lg:h-1/2 bg-cover bg-center rounded-lg shadow-md px-4 py-3 flex items-end">
+                <div style="background-image: url('feature/2.webp');" class="w-full h-36 lg:h-1/2 bg-cover bg-center rounded-lg shadow-md px-4 py-3 flex items-end">
                     <div class="bg-white w-full h-max py-1 px-3 rounded-lg flex flex-row justify-between shadow-md items-center">
                         <div class="flex flex-col">
                             <p class="font-medium">Cluster Nirwana</p>
@@ -31,7 +31,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="bg-[url('feature/3.jpg')] w-full h-36 lg:h-1/2 bg-cover bg-center rounded-lg shadow-md px-4 py-3 flex items-end">
+                <div style="background-image: url('feature/3.jpg');" class="w-full h-36 lg:h-1/2 bg-cover bg-center rounded-lg shadow-md px-4 py-3 flex items-end">
                     <div class="bg-white w-full h-max py-1 px-3 rounded-lg flex flex-row justify-between shadow-md items-center">
                         <div class="flex flex-col">
                             <p class="font-medium">Srikandi Housing</p>
